eb1379110a167977e9a99a80f6d56b514f14cd08
O Hung Lun <hunglun@speqtral.space>
2023-05-21 23:03:33 +0800

    added testbench for button led

--- udemy-button-led.srcs/sim_1/new/test_button_led.vhd
+++ udemy-button-led.srcs/sim_1/new/test_button_led.vhd
@@ -1,14 +1,14 @@
 ----------------------------------------------------------------------------------
--- Company: SpeQtral
--- Engineer: O Hung Lun
+-- Company: 
+-- Engineer: 
 -- 
--- Create Date: 21.05.2023 15:59:02
+-- Create Date: 21.05.2023 22:35:28
 -- Design Name: 
--- Module Name: BUTTON_LED - Behavioral
+-- Module Name: test_button_led - Behavioral
 -- Project Name: 
--- Target Devices: cmod-s7
+-- Target Devices: 
 -- Tool Versions: 
--- Description: a learning project to light up LED in response to button click
+-- Description: 
 -- 
 -- Dependencies: 
 -- 
@@ -21,7 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.numeric_std.all;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -31,21 +31,37 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity BUTTON_LED is
+entity test_button_led is
+--  Port ( );
+end test_button_led;
+
+architecture test of test_button_led is
+
+component BUTTON_LED
   generic (
-    NUM_BUTTONS : integer := 2
+  NUM_BUTTONS : integer := 2
   );
   Port ( 
     button : in std_logic_vector( NUM_BUTTONS - 1 downto 0);
     led: out std_logic_vector ( NUM_BUTTONS - 1 downto 0)
   );
+end component;
 
-end BUTTON_LED;
+signal button0 : std_logic := '0';
+signal button1 : std_logic := '0';
 
-architecture Behavioral of BUTTON_LED is
+signal led0 : std_logic := '0';
+signal led1 : std_logic := '0';
 
 begin
+    dev_to_test: BUTTON_LED
+    port map( button(0) => button0, button(1) => button1, led(0) => led0, led(1) => led1);
 
-    led <= button;
-
-end Behavioral;
+    button_stimulus : process
+    begin
+     wait for 1 ms;
+     button0 <= not button0;
+     wait for 1 ms;
+     button1 <= not button1;
+    end process button_stimulus;
+end test;
