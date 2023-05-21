006ebf97e2a4253610a3858b6c9a9e342b4d6247
O Hung Lun <hunglun@speqtral.space>
2023-05-21 16:04:13 +0800

    added project files

---
+++ udemy-button-led.srcs/sources_1/new/BUTTON_LED.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: SpeQtral
+-- Engineer: O Hung Lun
+-- 
+-- Create Date: 21.05.2023 15:59:02
+-- Design Name: 
+-- Module Name: BUTTON_LED - Behavioral
+-- Project Name: 
+-- Target Devices: cmod-s7
+-- Tool Versions: 
+-- Description: a learning project to light up LED in response to button click
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity BUTTON_LED is
+--  Port ( );
+end BUTTON_LED;
+
+architecture Behavioral of BUTTON_LED is
+
+begin
+
+
+end Behavioral;
